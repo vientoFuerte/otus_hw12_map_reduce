25c9f0a5637ad5bce49c67e8f71791c3322c90db
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-05-04 15:12:53 +0300

    Update mapper.cpp
    Отлажен вывод 9 столбца - цены.

--- mapper.cpp
+++ mapper.cpp
@@ -1,13 +1,45 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
+std::string getColumn(std::string str, uint colNum)
+{
+    int delimCnt = 0;
+    size_t start = 0;
+    size_t stop = str.length();
+    bool startFound = false;  
+
+    std::string field;
+
+    for (size_t i=0; i<str.length(); i++)
+    {
+        if(str[i] == ',') {
+            delimCnt++;
+        }
+        if(delimCnt == colNum && !startFound) {
+
+            start = i;  // запятая перед нужным полем
+            startFound = true;
+        }
+        else if (startFound && delimCnt == colNum +1)  {
+            stop = i - start-1; // количество символов не включая запятые
+            break;
+            
+        }
+    }
+
+    field = str.substr(start+1, stop);
+    return field;
+}
 
 int main(int argc, char ** argv)
 {
     std::string line;
-    while (std::getline(std::cin, line))
-    {
-        std::cout << line << "\t" << "1" << std::endl;
+  
+   if (std::getline(std::cin, line)) 
+   {
+        //std::cout << line << std::endl;
+        std::cout << getColumn(line, 9) << std::endl;
     }
 
     return 0;
